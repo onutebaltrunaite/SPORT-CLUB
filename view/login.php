ddba589ebc746d4112145000ffc6151a4d5b6f14
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-03-01 19:37:23 +0200

    login form added

---
+++ view/login.php
@@ -0,0 +1,20 @@
+<h2 class="text-center mt-5">Prisijungti</h2>
+<div class="row">
+    <div class="col-md-6 mx-auto text-center">
+        <div class="card card-body bg-info mt-5">
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="email">Įveskite savo el. pašto adresą:</label>
+                    <input type="text" name="email" id="email" class="form-control form-control-lg" value="">
+                </div>
+                <div class="form-group">
+                    <label for="password">Įveskite savo slaptažodį:</label>
+                    <input type="password" name="password" id="password" class="form-control form-control-lg" value="">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-secondary">Prisijungti</button>            
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
