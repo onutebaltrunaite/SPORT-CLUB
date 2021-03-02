c2bfc164cb1b0a45c01e7e26275c878c5bf06ea7
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-03-02 16:31:45 +0200

    styled with css main index page

--- view/home.php
+++ view/home.php
@@ -1,3 +1,37 @@
 
-<h1><?php echo $name; ?> - home page </h1>
-<p><?php echo $subtitle; ?></p>
+
+<div class="bg-img">
+    
+</div>
+
+<div class="services d-flex justify-content-center align-items-center">
+
+    <div class="card" style="width: 25%;">
+        <img class="card-img-top" src="https://assets.entrepreneur.com/content/3x2/2000/1595856981-GettyImages-1180359623.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Group classes</h5>
+            <p class="card-text">Details...</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 25%;">
+        <img class="card-img-top" src="http://www.hellomissniki.com/wp-content/uploads/2019/10/personal-trainer-1-compressor.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Pilates classes</h5>
+            <p class="card-text">Details...</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 25%;">
+        <img class="card-img-top" src="https://assets.entrepreneur.com/content/3x2/2000/1595856981-GettyImages-1180359623.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Group classes</h5>
+            <p class="card-text">Details...</p>
+        </div>
+    </div>
+
+</div>
+
+<div class="map">zemelapis</div>
+
+
