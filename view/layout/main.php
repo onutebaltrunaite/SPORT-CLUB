9c24fb3b580e4b640a6bcdef10b1b42f36492c61
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-03-03 03:27:33 +0200

    footer and navbar fix

--- view/layout/main.php
+++ view/layout/main.php
@@ -14,50 +14,30 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-<!-- <a class="navbar-brand" href="#">Gym <strong>Plus++</strong> <i class="fas fa-dumbbell"></i></a> -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link" href="/">Home</a>
-            <a class="nav-link" href="/about">About Us</a>
-            <a class="nav-link" href="/feedbacks">Feedbacksai</a>
-            <!-- <a class="nav-link" href="/contact">Contact</a> -->
-        </div>
-        <div class="navbar-nav">
-        <?php if(!\app\core\Session::isUserLoggedIn()) : ?>
-            <a class="nav-link" href="/login">Login</a>
-            <a class="nav-link" href="/register">Register</a>
-        <?php else : ?>   
-            <a class="nav-item nav-link disabled" href="#"><?php echo $_SESSION['user_name']; ?></a>
-            <a class="nav-link" href="/logout">Logout</a>
-        <?php endif; ?>   
-        </div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-center">
+            <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link" href="/feedbacks">About Us</a>
+                </div>
+                <div class="navbar-nav">
+                    <?php if(!\app\core\Session::isUserLoggedIn()) : ?>
+                        <a class="nav-link" href="/login">Login</a>
+                        <a class="nav-link" href="/register">Register</a>
+                    <?php else : ?>   
+                        <a class="nav-item nav-link disabled" href="#"><?php echo $_SESSION['user_name']; ?></a>
+                        <a class="nav-link" href="/logout">Logout</a>
+                    <?php endif; ?>   
+                </div>
+            </div>        
+    </nav>
+
+    <div class="content">
+        {{content}}
     </div>
-</nav>
 
-
-<div class="container">
-    {{content}}
-</div>
-
-
-
-
-<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"></script>
-
-
-
-    <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom">
-        <div class="container text-center">
+    <footer id="footer" class="py-4 bg-dark text-white-50" style="height: 70px;">
+        <div class="cont text-center">
             <small> &copy; <?php echo date('Y');?>. Onute Baltrunaite, all rights reserved.</small>
         </div>
     </footer>
