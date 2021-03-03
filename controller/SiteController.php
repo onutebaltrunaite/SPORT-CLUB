7f09ee8ea8c5079529f40df51c109b6f354d5ec7
baltrunaiteo@gmail.com <baltrunaiteo@gmail.com>
2021-03-03 10:56:20 +0200

    deleted pages that r not needed

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -19,49 +19,10 @@ class SiteController extends Controller
     {
         $params = [
             'name' => "AlmostLara",
-            'subtitle' => "This is a nice way to learn PHP"
+            'subtitle' => "This is a nice way to learn PHP",
         ];
 
         return $this->render('home', $params);
     }
-    /**
-     * This serves the about form view
-     * @return string
-     */
-    public function about()
-    {
-        $params = [
-            'version' => "1.0.0",
-        ];
-        // lets render view
-        return $this->render('about', $params);
-    }
 
-    /**
-     * This serves the contact form view
-     * @return string
-     */
-    public function contact()
-    {
-        // lets render view
-        return $this->render('contact');
-    }
-
-    /**
-     * This is were we handle post contact form.
-     *
-     * @return string
-     */
-    public function handleContact(Request $request)
-    {
-        // we user get body method to see user input
-        $body = $request->getBody();
-
-        echo "<pre>";
-        var_dump($body);
-        echo "</pre>";
-        exit;
-
-        return "handling form from site controller handle form method";
-    }
 }
--- view/about.php
+++
@@ -1,3 +0,0 @@
-<h1>THis is about page</h1>
-<p>really about</p>
-<h3>And Version is <?php echo $version ?? ''; ?></h3>
